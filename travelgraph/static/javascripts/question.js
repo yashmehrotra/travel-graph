6bcbf460b6bf5e9f1e6634b73cec3d7d3462dbeb
Avijit Gupta <526avijitgupta@gmail.com>
2015-02-15 15:30:40 +0530

    google-maps for ckeditor + other changes

--- travelgraph/static/javascripts/question.js
+++ travelgraph/static/javascripts/question.js
@@ -1,25 +1,40 @@
-function submit() {
-    var question = $('#question').val();
-    var ques_des = $('#question_desc').val();
-    var ques_tag = $('#question_tags').val();
+$(function (){
+        
+  $('#question-submit-button').on('click',function(){
+    var ques_text = $('#question-text').val();
+    var ques_desc = objEditor.getData();
+    var ques_tags = $('#question-tags').val();
+      
+    alert(ques_text);
+    alert(ques_desc);
+    alert(ques_tags);
 
-    $.ajax({
-        type: "POST",
-        url: "api/content/add_question",
-        data: {
-            'question_text':question,
-            'question_desc':ques_des,
-            'question_tags':ques_tag,
-        },
-        success: function(result) {
-            if(result) {
-                console.log(result);
-                result = JSON.parse(result);
-                console.log(result['status']);
-                console.log(result.message);
-            } else {
-                console.log('Problem with ajax');
-            }
-        }
-    });
+    submit(ques_text, ques_desc, ques_tags);
+  });
+
+});
+
+function submit(ques_text, ques_desc, ques_tags) {
+  alert("Submitting");
+  $.ajax({
+    type: "POST",
+    url: "api/content/add_question",
+    data: {
+      'question_text': ques_text,
+      'question_desc': ques_desc,
+      'question_tags': ques_tags,
+    },
+    success: function(result) {
+      if(result) {
+        console.log(result);
+        result = JSON.parse(result);
+        console.log(result['status']);
+        console.log(result.message);
+      } else {
+        console.log('Problem with ajax');
+        alert('Problem with ajax');
+      }
+    }
+  });
+  return false;
 }
--- travelgraph/static/plugins/ckeditor/config.js
+++ travelgraph/static/plugins/ckeditor/config.js
@@ -35,4 +35,8 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+	
+	// Addition of Google Maps plugin.
+	config.extraPlugins = 'wenzgmap';
+
 };
