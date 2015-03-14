19e5a5320ca3ceffc49168f858406997dc6d22fc
Avijit Gupta <526avijitgupta@gmail.com>
2015-03-14 21:05:59 +0530

    undo fuck up by avijit

---
+++ travelgraph/controllers.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask, request, Response
+from flask import render_template, url_for, redirect, send_from_directory
+from flask import send_file, make_response, abort
+
+from travelgraph import app
+
+# routing for basic pages (pass routing onto the Angular app)
+@app.route('/')
+@app.route('/login')
+@app.route('/blog')
+def basic_pages(**kwargs):
+	return make_response(open('travelgraph/templates/index.html').read())
