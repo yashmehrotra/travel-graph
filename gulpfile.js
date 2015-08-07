0410cc2468f9ac2d2b4173d23383f24c416fb925
Avijit Gupta <526avijit@gmail.com>
2015-08-08 02:17:45 +0530

    Working gulp, install packages through bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,65 @@
 'use strict';
 
-// requirements
+// Lib Imports
+var path = require('path');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var filesize = require('gulp-filesize');
+var less = require('gulp-less');
+var changed = require('gulp-changed');
+var watch = require('gulp-watch');
 
-var gulp = require('gulp'),
-    browserify = require('gulp-browserify'),
-    size = require('gulp-size'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify');
+// File Paths
+var path_build = './travelgraph/static/build/';
+var path_js = './travelgraph/static/js/**/*.js';
+var path_less = './travelgraph/static/less/*.less';
 
-// tasks
-
-gulp.task('transform', function () {
-  return gulp.src('./travelgraph/static/javascripts/main.js')
-    .pipe(gulp.dest('./travelgraph/static/javascripts/src/'))
-    .pipe(size());
-});
-
-gulp.task('uglify-minify', function() {
-    return gulp.src('./travelgraph/static/javascripts/main.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./travelgraph/static/javascripts/src/main.js'));
-});
-
+// Tasks
 gulp.task('clean', function () {
-  return gulp.src(['./travelgraph/static/javascripts/src'], {read: false})
-    .pipe(clean())
+  return gulp.src(path_build, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('vendor', function() {
+  return gulp.src(path_js)
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(path_build))
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest(path_build))
+    .on('error', gutil.log)
+});
+
+gulp.task('css', function () {
+  return gulp.src(path_less)
+    .pipe(changed(path_build))
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest(path_build))
+    .on('error', gutil.log);
+});
+
+gulp.task('css:watch', function () {
+  watch({
+    glob: path_less,
+    emit: 'one',
+    emitOnGlob: false
+  }, function(files) {
+    return files
+      .pipe(less({
+        paths: [ path.join(__dirname, 'less', 'includes') ]
+      }))
+      .pipe(gulp.dest(path_build))
+      .on('error', gutil.log);
+  });
 });
 
 gulp.task('default', ['clean'], function () {
-  gulp.start('transform');
-  gulp.watch('./travelgraph/static/javascripts/main.js', ['transform']);
+  gulp.start('vendor');
+  gulp.watch(path_js, ['vendor']);
 });
