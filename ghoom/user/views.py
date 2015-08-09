e7dab80a22991260cb3ffb603ff31b9a4f6f22b5
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-08-09 16:35:57 +0530

    Lets get started

---
+++ ghoom/user/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request
+import random
+
+from ghoom.models import (
+    DbUser
+)
+
+api = Blueprint('api', __name__)
+
+@api.route('/user/<user_id>', methods=['GET', 'POST', 'PUT'])
+def rest_user(user_id=None):
+    if request.method == 'GET':
+        if user_id:
+            # Serialize user, add params for all data,meta data etc.
+            user = session.query(DbUser).get(user_id)
+            return user
+        else:
+            return None
+    elif request.method == 'POST':
+        # Take all the params and add a new user
+        return None
+    elif request.method == 'PUT':
+        # Edit the user
+        return None
