e07fcd2a4d1cbc72b2ce23bed44cd69288f0c6b7
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-08-12 19:56:05 +0530

    Added development module for ease of use

--- ghoom/__init__.py
+++ ghoom/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 # from werkzeug.contrib.fixers import ProxyFix
 from ghoom.user.views import api
+from ghoom.development.views import developer
 
 app = Flask(__name__)
 
@@ -8,3 +9,4 @@ app.secret_key = 'A0Zr98j/asd R~XHH!jmN]LWX/,?RT'
 # app.wsgi_app = ProxyFix(app.wsgi_app)
 
 app.register_blueprint(api, url_prefix='/api')
+app.register_blueprint(developer, url_prefix='/developer')
---
+++ ghoom/development/views.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, request
+
+from ghoom.user.utils import (
+    generate_auth_key,
+    generate_access_token,
+    generate_username
+)
+
+from ghoom.models import (
+    DbRequestKey,
+    DbUser,
+    session
+)
+
+from ghoom.helpers import response_json
+
+developer = Blueprint('developer', __name__)
+
+
+@developer.route('/init', methods=['POST'])
+def developer_init():
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+
+    username = generate_username(first_name, last_name)
+
+    user = DbUser(username=username,
+                  email=email,
+                  first_name=first_name,
+                  last_name=last_name)
+
+    session.add(user)
+    session.commit()
+
+    req_key_ttl = 60 * 60 * 24 * 31
+    req_key_obj = DbRequestKey(request_key="SUPERDBADMIN",
+                               type="development",
+                               ttl=req_key_ttl)
+    session.add(req_key_obj)
+    session.commit()
+
+    auth_key = generate_auth_key(req_key_obj.request_key)
+    access_token = generate_access_token(user.id, auth_key)
+
+    response = {
+        'user': {
+            'id': user.id,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+        },
+        'auth_key': auth_key,
+        'access_token': access_token
+    }
+
+    return response_json(response)
