11e853bc1e1aba52bb33dd9a31d3818c104e5486
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-08-16 18:23:00 +0530

    setup.sh

---
+++ bin/setup.sh
@@ -0,0 +1,21 @@
+echo "Updating"
+sudo apt-get update > /dev/null
+
+echo "Installing Nginx"
+sudo apt-get install -y nginx > /dev/null
+
+echo "Installing Dependencies"
+# sudo apt-get install -y libpq-dev python-dev python-psycopg2 > /dev/null
+
+echo "Installing pip"
+sudo apt-get install -y python-pip > /dev/null
+
+# Install virtualenv yourself
+
+# Install PostgreSQL
+
+# Install Redis
+
+# Install requirements.txt
+
+# Initialize Database through manager
