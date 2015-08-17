ba303a7241178b553c861e1013e37960bbe92aea
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-08-18 00:35:49 +0530

    cors fix

--- verak/__init__.py
+++ verak/__init__.py
@@ -16,3 +16,17 @@ app.secret_key = 'A0Zr98j/asd R~XHH!jmN]LWX/,?RT'
 app.register_blueprint(api_user, url_prefix='/api/user')
 app.register_blueprint(developer, url_prefix='/developer')
 app.register_blueprint(api_question, url_prefix='/api/question')
+
+@app.after_request
+def add_cors(resp):
+    """ Ensure all responses have the CORS headers. This ensures any failures are also accessible
+        by the client. """
+    resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin','*')
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get( 
+        'Access-Control-Request-Headers', 'Authorization' )
+    # set low for debugging
+    if app.debug:
+        resp.headers['Access-Control-Max-Age'] = '1'
+    return resp
