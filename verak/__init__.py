a1400ee8f6636374a2744af2a2c1baa859bd56d5
Avijit Gupta <526avijit@gmail.com>
2015-08-17 20:24:20 +0530

    Added CORS for flask

--- verak/__init__.py
+++ verak/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask.ext.cors import CORS
 # from werkzeug.contrib.fixers import ProxyFix
 from verak.user.views import api_user
 from verak.development.views import developer
 from verak.doobie.views import api_question
 
 app = Flask(__name__)
-
+# For allowing Cross-Origin-Resource Sharing
+# Should not go into production
+# Research about it
+CORS(app)
 app.secret_key = 'A0Zr98j/asd R~XHH!jmN]LWX/,?RT'
 # app.wsgi_app = ProxyFix(app.wsgi_app)
 
