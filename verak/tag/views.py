b38a252a0ecae41f6789dd10a54c59b98228b1d0
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-08-23 18:51:24 +0530

    Added view for getting doobie through tags

--- verak/tag/views.py
+++ verak/tag/views.py
@@ -1,1 +1,40 @@
-# Add stuff here
+from flask import Blueprint, request
+
+from verak.models import (
+    DbTag,
+    DbDoobieTagMapping,
+    session
+)
+
+from verak.tag.utils import get_tag_id
+
+api_tag = Blueprint('api_tag', __name__)
+
+
+@api_tag.route('/<tag>/', methods=['GET'])
+@auth_required
+@login_required
+def tag_view(tag):
+    """
+    Returns doobies corresponding to tag
+    """
+
+    tag = tag.lower()
+
+    tag_id = get_tag_id(tag)
+
+    doobies = session.query(DbDoobieTagMapping).\
+                filter(DbDoobieTagMapping.tag_id == tag_id).\
+                all()
+
+    doobie_ids = []
+
+    for d in doobies:
+        doobie_ids.append(d.doobie_id)
+
+    response = {
+        'status': 'success',
+        'doobies': doobie_ids
+    }
+
+    return response_json(response)
