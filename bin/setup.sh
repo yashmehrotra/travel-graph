81596fcbc72524fdfc6b6ef0758abdd1b943cffa
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-09-05 23:33:24 +0530

    Minor Changes

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,7 +7,7 @@ echo "Installing Nginx"
 sudo apt-get install -y nginx > /dev/null
 
 echo "Installing Dependencies"
-# sudo apt-get install -y libpq-dev python-dev python-psycopg2 > /dev/null
+sudo apt-get install -y libpq-dev python-dev python-psycopg2 > /dev/null
 
 echo "Installing pip"
 sudo apt-get install -y python-pip > /dev/null
@@ -29,7 +29,7 @@ make
 make install
 cd utils
 sudo ./install_server.sh
-
+cd $ROOT
 sudo service redis_6379
 
 # Install requirements.txt
