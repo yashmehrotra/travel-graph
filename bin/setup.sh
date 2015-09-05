4af0451d13a64d20931bfa997c8a8cf902db4eeb
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-09-05 14:17:49 +0530

    Updated setup shell script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,5 @@
+ROOT=`pwd`
+
 echo "Updating"
 sudo apt-get update > /dev/null
 
@@ -13,8 +15,22 @@ sudo apt-get install -y python-pip > /dev/null
 # Install virtualenv yourself
 
 # Install PostgreSQL
+echo "Installing PostgreSQL"
+sudo apt-get install -y postgresql postgresql-contrib > /dev/null
+sudo -u postgres createuser -s -P db_admin
+createdb -h localhost -p 5432 -U db_admin travelgraph
 
 # Install Redis
+echo "Installing Redis"
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+make install
+cd utils
+sudo ./install_server.sh
+
+sudo service redis_6379
 
 # Install requirements.txt
 
