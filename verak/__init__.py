cd415686692e89a9c5453079e5d67fb17e1a404c
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-09-08 19:39:26 +0530

    Integrating flask restful

--- verak/__init__.py
+++ verak/__init__.py
@@ -1,16 +1,15 @@
 from flask import Flask, request
 import json
-# from werkzeug.contrib.fixers import ProxyFix
+
 from verak.user.views import api_user
-from verak.development.views import developer
+from verak.development.views import developer_blueprint
 from verak.doobie.views import api_question
 
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/asd R~XHH!jmN]LWX/,?RT'
-# app.wsgi_app = ProxyFix(app.wsgi_app)
 
 app.register_blueprint(api_user, url_prefix='/api/user')
-app.register_blueprint(developer, url_prefix='/developer')
+app.register_blueprint(developer_blueprint, url_prefix='/developer')
 app.register_blueprint(api_question, url_prefix='/api/question')
 
 # CORS Settings
--- verak/development/views.py
+++ verak/development/views.py
@@ -15,54 +15,13 @@ from verak.models import (
 
 from verak.helpers import response_json
 
-developer = Blueprint('developer', __name__)
-developer_frf = Api(developer)
-
-@developer.route('/init', methods=['POST'])
-def developer_init():
-
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    email = request.form['email']
-
-    username = generate_username(first_name, last_name)
-
-    user = DbUser(username=username,
-                  email=email,
-                  first_name=first_name,
-                  last_name=last_name)
-
-    session.add(user)
-    session.commit()
-
-    req_key_ttl = 60 * 60 * 24 * 31
-    req_key_obj = DbRequestKey(request_key="SUPERDBADMIN",
-                               type="development",
-                               ttl=req_key_ttl)
-    session.add(req_key_obj)
-    session.commit()
-
-    auth_key = generate_auth_key(req_key_obj.request_key)
-    access_token = generate_access_token(user.id, auth_key)
-
-    response = {
-        'user': {
-            'id': user.id,
-            'username': user.username,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'email': user.email
-        },
-        'auth_key': auth_key,
-        'access_token': access_token
-    }
-
-    return response_json(response)
+developer_blueprint = Blueprint('developer', __name__)
+api_developer = Api(developer_blueprint)
 
 
 class DeveloperInitFRFTest(Resource):
     """
-    Testing Flask Restful
+    Made for instantly making an account
     """
 
     def post(self):
@@ -104,4 +63,4 @@ class DeveloperInitFRFTest(Resource):
 
         return response_json(response)
 
-developer_frf.add_resource(DeveloperInitFRFTest, '/initfrf/')
+api_developer.add_resource(DeveloperInitFRFTest, '/init/')
