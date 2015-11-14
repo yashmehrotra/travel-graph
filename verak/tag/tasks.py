44245cb5f50752ebab3927291a1dce955dc26690
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-11-14 12:10:52 +0530

    Improved tag edit updation

--- verak/tag/tasks.py
+++ verak/tag/tasks.py
@@ -52,11 +52,11 @@ def map_tags_to_doobie(tags, doobie_id):
 
     disable_query = session.query(DbDoobieTagMapping).\
                         filter(DbDoobieTagMapping.tag_id.in_(ids_to_disable),
-                               DbDoobieTagMapping.doobie_id == doobie_id).\
-                        update({DbDoobieTagMapping.enabled: False},
-                               synchronize_session=False)
+                               DbDoobieTagMapping.doobie_id == doobie_id)
+    if disable_query:
+        disable_query = disable_query.update({DbDoobieTagMapping.enabled: False},
+                                             synchronize_session=False)
 
-    if disable_query > 0:
         session.commit()
 
     return True
